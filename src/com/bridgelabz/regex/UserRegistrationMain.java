0b46da352b7875e23c1c0149019229f7521a518c
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-10 15:54:13 +0530

    Initialized the program

---
+++ src/com/bridgelabz/regex/UserRegistrationMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.regex;
+
+import java.util.Scanner;
+
+public class UserRegistrationMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration using Regex");
+	}
+
+}
